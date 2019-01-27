f4d00866e8aaf2c1d3f966a60ba26a6b386c0488
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-27 18:27:22 +0000

    Deploy avin/pixel-mania to github.com/avin/pixel-mania.git:gh-pages

---
+++ precache-manifest.ff40a68ac1ea02fe4cfd82d10dfb78f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "047b8d4833dc14810055",
+    "url": "./static/css/main.dad5d25b.chunk.css"
+  },
+  {
+    "revision": "047b8d4833dc14810055",
+    "url": "./static/js/main.047b8d48.chunk.js"
+  },
+  {
+    "revision": "f2e8ea776a7a369244e3",
+    "url": "./static/css/1.ac3ad404.chunk.css"
+  },
+  {
+    "revision": "f2e8ea776a7a369244e3",
+    "url": "./static/js/1.f2e8ea77.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2f3ba57c790adb9f0880ddf0c11fb6ed",
+    "url": "./index.html"
+  }
+];
